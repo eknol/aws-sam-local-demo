04f2af5869ab2766fdd005f62c4403c8f9f57b79
Edze Knol <eknol@xebia.com>
2018-09-26 09:59:52 +0200

    Added docker-compose file and initial setup for local dynamodb

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # aws-sam-local-demo
+
+
+docker pull amazon/dynamodb-local
+docker run -p 8000:8000 amazon/dynamodb-local
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+// const AWS = require('aws-sdk');
+const shell = require('gulp-shell');
 
-gulp.task('default', function() {
+gulp.task('default', function(done) {
   // place code for your default task here
   console.log("Hooray it works!! ");
+  done();
 });
+
+gulp.task('show-tables',
+    shell.task('aws dynamodb list-tables --endpoint-url http://localhost:8000'
+));
+
+gulp.task('create-tables',
+    shell.task('aws dynamodb create-table --cli-input-json file://dynamodb/config/tables/table.json --endpoint-url http://localhost:8000'
+));
