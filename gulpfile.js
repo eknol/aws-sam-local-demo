5ddbb5c4a0336a7c2a0762f8d56b9fa0b899eca4
Edze Knol <eknol@xebia.com>
2018-10-01 21:06:16 +0200

    Changed dynamo image to one that accepts cl-flags

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,24 @@
 const gulp = require('gulp');
-// const AWS = require('aws-sdk');
 const shell = require('gulp-shell');
 
 gulp.task('default', function(done) {
-  // place code for your default task here
-  console.log("Hooray it works!! ");
+  console.log("Hello :)");
   done();
 });
 
 gulp.task('show-tables',
-    shell.task('aws dynamodb list-tables --endpoint-url http://localhost:8000'
-));
+    shell.task('aws dynamodb list-tables --endpoint-url http://localhost:8000')
+);
 
 gulp.task('create-tables',
-    shell.task('aws dynamodb create-table --cli-input-json file://dynamodb/config/tables/table.json --endpoint-url http://localhost:8000'
-));
+    shell.task('aws dynamodb create-table --cli-input-json file://dynamodb/config/tables/table.json --endpoint-url http://localhost:8000')
+);
 
 gulp.task('add-data',
-    shell.task('aws dynamodb put-item --table-name fruitsTable --item file://dynamodb/config/tables/data.json --return-consumed-capacity TOTAL --endpoint-url http://localhost:8000'
-));
+    shell.task('aws dynamodb put-item --table-name fruitsTable --item file://dynamodb/config/tables/data.json --return-consumed-capacity TOTAL --endpoint-url http://localhost:8000')
+);
 
 gulp.task('get-data',
-    shell.task('aws dynamodb get-item --table-name fruitsTable --key file://dynamodb/config/tables/key.json --endpoint-url http://localhost:8000'
-));
+    shell.task('aws dynamodb get-item --table-name fruitsTable --key file://dynamodb/config/tables/key.json --endpoint-url http://localhost:8000')
+);
 
